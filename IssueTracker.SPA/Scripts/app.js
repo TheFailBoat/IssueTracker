308e15de52f10c742662670896d32cafd61782f3
Will Smith <william.smith12@imperial.ac.uk>
2013-06-17 01:02:54 +0100

    Added auth to SPA

--- IssueTracker.SPA/Scripts/app.js
+++ IssueTracker.SPA/Scripts/app.js
@@ -1,14 +1,44 @@
 ﻿'use strict';
 
 /* App Module */
+var apiBase = "/IssueTracker.API";
 
 angular.module('issueTracker', ['restangular', 'issueTrackerFilters']).
-  config(['$routeProvider', function ($routeProvider) {
+  config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
       $routeProvider.
           when('/issues', { templateUrl: 'templates/issues/list.html', controller: IssuesCtrl }).
           when('/issues/:id', { templateUrl: 'templates/issues/detail.html', controller: IssueDetailCtrl }).
+          when('/login', { templateUrl: 'templates/auth/login.html', controller: LoginCtrl }).
+          when('/logout', { controller: LogoutCtrl }).
           otherwise({ redirectTo: '/issues' });
+
+      //TODO $locationProvider.html5Mode(true);
   }])
   .config(function (RestangularProvider) {
-      RestangularProvider.setBaseUrl("/IssueTracker.API");
+      RestangularProvider.setBaseUrl(apiBase);
+      RestangularProvider.setErrorInterceptor(function (response) {
+          // TODO handle general error
+      });
+  })
+  .run(function ($http, $rootScope, $location) {
+      $rootScope.dateFormat = 'yyyy-MM-dd HH:mm';
+
+      $rootScope.isAuthed = false;
+      $rootScope.auth = {};
+
+      checkAuth($http, $rootScope, $location);
   });
+
+function checkAuth($http, $rootScope, $location) {
+    $http({ method: 'GET', url: apiBase + '/me' })
+        .success(function (data) {
+            $rootScope.isAuthed = true;
+            $rootScope.auth = data;
+        })
+        .error(function () {
+            $rootScope.isAuthed = false;
+            $rootScope.auth = {};
+
+            $location.path("/login");
+        });
+}
--- IssueTracker.SPA/Scripts/controllers.js
+++ IssueTracker.SPA/Scripts/controllers.js
@@ -1,7 +1,45 @@
 ﻿'use strict';
 
+// General
+function MainNavCtrl($scope, $location) {
+    $scope.location = $location;
+    $scope.links = [
+        { url: "#/issues", name: "List Issues" },
+        { url: "#/issues/create", name: "Create Issue" }
+    ];
+}
+
+// Auth
+function LoginCtrl($scope, $http, $rootScope, $location) {
+    $scope.username = "";
+    $scope.password = "";
+    $scope.error = false;
+
+    $scope.login = function () {
+        $scope.error = false;
+
+        $http.post(apiBase + '/auth/credentials', { username: $scope.username, password: $scope.password })
+            .success(function () {
+                $location.path('/');
+                checkAuth($http, $rootScope, $location);
+            })
+            .error(function () {
+                $scope.password = "";
+                $scope.error = true;
+            });
+    };
+}
+
+function LogoutCtrl() {
+
+}
+
+// Issues
 function ProcessIssue(issue) {
     issue.CreatedAt = new Date(parseInt(issue.CreatedAt.substr(6)));
+    if (issue.UpdatedAt) {
+        issue.UpdatedAt = new Date(parseInt(issue.UpdatedAt.substr(6)));
+    }
 
     return issue;
 }
@@ -24,7 +62,32 @@ function IssueDetailCtrl($scope, $routeParams, Restangular) {
 }
 
 function IssuePersonCtrl($scope, Restangular) {
-    Restangular.one('person', $scope.$parent.issue.ReporterId).get().then(function (person) {
+    Restangular.one('person', $scope.issue.ReporterId).get().then(function (person) {
         $scope.person = person;
     });
+}
+function IssueCustomerCtrl($scope, Restangular) {
+    if ($scope.issue.CustomerId) {
+        Restangular.one('customers', $scope.issue.CustomerId).get().then(function (customer) {
+            $scope.customer = customer;
+        });
+    }
+}
+function IssueCategoryCtrl($scope, Restangular) {
+    Restangular.one('categories', $scope.issue.CategoryId).get().then(function (category) {
+        $scope.category = category;
+    });
+}
+function IssueStatusCtrl($scope, Restangular) {
+    Restangular.one('statuses', $scope.issue.StatusId).get().then(function (status) {
+        $scope.status = status;
+    });
+}
+function IssuePriorityCtrl($scope, Restangular) {
+    Restangular.one('priorities', $scope.issue.PriorityId).get().then(function (priority) {
+        $scope.priority = priority;
+    });
+}
+function IssueProgressCtrl($scope, Restangular) {
+    $scope.progress = $scope.issue.Progress;
 }
