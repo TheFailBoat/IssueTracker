7cc679f3c287071cc57e729d0405c478ed033375
Will Smith <will@thefailboat.co.uk>
2013-07-24 21:16:52 +0100

    Started V2 SPA

---
+++ issuetracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Issuetracker::Application.config.secret_token = 'eb12be412298dd09df936c9d04b7ba588e6a93f2c2a2e4ac7ea0e94a4cd8aca43a40e42e9b2bcfc14ac65806e82a2c21b2418b94596f9cfb4eca8179df4dcf20'
