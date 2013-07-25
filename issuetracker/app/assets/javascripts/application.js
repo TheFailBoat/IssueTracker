b940f0dfa9a962f4c9cae828a96607efe515f60d
Will Smith <will@thefailboat.co.uk>
2013-07-26 00:14:04 +0100

    Bootstrapped SPA

--- issuetracker/app/assets/javascripts/application.js
+++ issuetracker/app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require issuetracker
+App = Ember.Application.create();
+
 //= require_tree .
+
+$(document).foundation();
